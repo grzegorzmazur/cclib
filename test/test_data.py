2adf7e63cd6fce090b67ac94423d0a19065fd149
Grzegorz Mazur <teoretyk@gmail.com>
2018-06-28 14:09:38 +0200

    basic tests of the niedoida parser

--- test/test_data.py
+++ test/test_data.py
@@ -27,7 +27,7 @@ sys.path.insert(1, os.path.join(__filedir__, 'data'))
 
 parser_names = [
     "ADF", "DALTON", "GAMESS", "GAMESSUK", "Gaussian", "Jaguar", "Molcas", "Molpro",
-    "MOPAC", "NWChem", "ORCA", "Psi", "QChem",
+    "MOPAC", "Niedoida", "NWChem", "ORCA", "Psi", "QChem",
 ]
 all_parsers = {name: getattr(cclib.parser, name) for name in parser_names}
 
